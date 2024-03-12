c7a6b3b8056d43c82e68902b07eb7988741873c7
Priyanshu Ranjan <117588084+priyanshuuranjan@users.noreply.github.com>
2024-03-12 23:43:43 +0530

    Time: 11 ms (44.53%), Space: 7.1 MB (62.30%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        // base case
-        if(n == 0)
-            return 0;
-        if(n == 1)
-            return 1;
-        
-        int ans = fib(n-1)+ fib(n-2);
+        //base case 
+        if (n == 0)
+        return 0;
+        if (n == 1)
+        return 1;
+      //Recursive Call
+        int ans = fib(n-1) + fib(n-2);
         return ans;
+
     }
 };
