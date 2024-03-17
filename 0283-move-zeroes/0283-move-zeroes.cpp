cc1c85d829bc4ada4d3ac170bb160e318f590be9
Priyanshu Ranjan <117588084+priyanshuuranjan@users.noreply.github.com>
2024-03-17 19:46:50 +0530

    Time: 17 ms (46.45%), Space: 21.7 MB (11.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,38 @@
+// best solution 
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                for (int j = i + 1; j < n; j++) {
-                    if (nums[j] != 0) {
-                        swap(nums[i], nums[j]);
-                        break;
-                    }
-                }
+        // jitne v non zero number hai unko ye left me shift kra rhe hai
+        
+        int i=0;
+        for(int j=0; j<nums.size(); j++){
+            if(nums[j] != 0){
+                swap(nums[j], nums[i]);
+                 i++;
             }
+           
         }
+        
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int n = nums.size();
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] == 0) {
+//                 for (int j = i + 1; j < n; j++) {
+//                     if (nums[j] != 0) {
+//                         swap(nums[i], nums[j]);
+//                         break;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// };
